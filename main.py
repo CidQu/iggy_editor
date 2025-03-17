04ecbdb3a5584a7ec3c7f834554f90aab22005d8
Yavuz Selimhan KAYA <40399630+CidQu@users.noreply.github.com>
2025-03-17 18:31:09 +0100

    v0.0.3
    Bug fixes and docs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def find_and_print_sequences(file_path):
     sequence = bytes.fromhex('C0688F6DF77F000020658F6DF77F0000A0658F6DF77F0000F0698F6DF77F')
     skip_bytes = 50
 
-    # We have two different character endings, if those are not exist that means our character list ended.
+    # We have four different character endings, if those are not exist that means our character list ended.
     # As of tody I have no idea why they're different. I believe there should be something different.
     character_ending_1 = bytes.fromhex('00000000000000000100010000000000')
     character_ending_2 = bytes.fromhex('00000000000000000000010000000000')
@@ -53,16 +53,23 @@ def find_and_print_sequences(file_path):
                 print(f"Font file offset: {start}") # Print offset of the font file start.
                 
                 charlist = []
-                if character.startswith(pattern_start):
+                if character.startswith(pattern_start): # This is to make sure we're in the correct offset
                     charlist.append(character)
                 else:
                     raise UnicodeDecodeError
                 
                 while True:
+                    # Now we need to find other chars, that means we need to search for every 32 byte.
                     char_position += 32
-                    character = content[first_char_position + char_position : first_char_position + char_position + 16] # Skip 20 bytes after
+                    character = content[first_char_position + char_position : first_char_position + char_position + 16] 
+
+                    # Then get the ending of that char
                     character_ending = content[first_char_position + char_position + 16 : first_char_position + char_position + 16 + 16]
+
+                    # If ending is not one of the approved endings, break loop
                     if character_ending not in [character_ending_1, character_ending_2, character_ending_3, character_ending_4]:
+                        if character.startswith(pattern_start): # Make sure we're not skipping any chars
+                            charlist.append(character)
                         break
                     if character.startswith(pattern_start):
                         charlist.append(character)
