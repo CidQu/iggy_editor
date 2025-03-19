400fc63d0387bd867a1e74d3a5ac6aa977018dda
Yavuz Selimhan KAYA <40399630+CidQu@users.noreply.github.com>
2025-03-19 13:41:49 +0100

    Update README and main.py for IGGY Viewer usage

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import json
 from models.readChars import parse_file 
 from models.app import FontViewerApp, runApp
 from helpers.byteToHex import convert_bytes_to_hex
+import sys
 
-file_path = "iggy/deneme.iggy" # Change this with your actual iggy file name
-
-parsed_result = parse_file(file_path) 
-
-with open("json/fonts.json", "w") as f:
-        json.dump(convert_bytes_to_hex(parsed_result), f, indent=4)
-
-runApp(file_path)
+if len(sys.argv) == 3:
+    mode = sys.argv[1]
+    file_path = sys.argv[2]
+    if mode == "view":
+        runApp(file_path)
+        sys.exit(0)
+    elif mode == "dump":
+        parsed_result = parse_file(file_path) 
+        with open("json/fonts.json", "w") as f:
+            json.dump(convert_bytes_to_hex(parsed_result), f, indent=4)
+        sys.exit(0)
+    elif mode == "test":
+        runApp("iggy/deneme.iggy")
+else:
+    print("Usage: python main.py [dump|view] <file_path>")
+    sys.exit(1)
